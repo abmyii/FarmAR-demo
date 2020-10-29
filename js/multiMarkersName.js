69499ac9aa26462ef22294dd368b1e4cf8c11658
Abdurrahmaan Iqbal <abdurrahmaaniqbal@hotmail.com>
2020-10-29 20:21:43 +0000

    Get indicator showing

--- js/multiMarkersName.js
+++ js/multiMarkersName.js
@@ -1,54 +1,24 @@
-//Multi Markers WebAR-AR.js and Aframe - Playing the Archive - Connected Environment CASA-UCL
+// Detect marker found and lost
+AFRAME.registerComponent('markerhandler', {
+	init: function () {
+		const marker = this.el;
 
-//Global Variable
-var markersURLArray=[];
-var markersNameArray=[];
+		marker.addEventListener("markerFound", ()=> {
+			var markerId = marker.id;
+			console.log('Marker Found: ', markerId);
 
-AFRAME.registerComponent('markers_start',{
-	init:function(){
-		var sceneEl = document.querySelector('a-scene');
-		var markerEl = document.createElement('a-marker');
-		markerEl.setAttribute('type','pattern');
-		markerEl.setAttribute('url',markersURLArray[k]);
-		markerEl.setAttribute('id',markersNameArray[k]);
+			document.getElementById('cursor').setAttribute(
+				'material', 'color', 'green'
+			);
+		});
 
-		markerEl.setAttribute('registerevents','');
-		sceneEl.appendChild(markerEl);
+		marker.addEventListener("markerLost",() =>{
+			var markerId = marker.id;
+			console.log('Marker Lost: ', markerId);
 
-		// Adding text to each marker
-		var textEl = document.createElement('a-entity');
-
-		textEl.setAttribute('id','text');
-		textEl.setAttribute('text',{color: 'red', align: 'center', value: 'Temperature: ' + (Math.floor(Math.random() * 10)+10), width: '5.5'});
-		textEl.object3D.position.set(2, 0.7, 0);
-		textEl.object3D.rotation.set(-90, 0, 0);
-
-		markerEl.appendChild(textEl);
-	}
+			document.getElementById('cursor').setAttribute(
+				'material', 'color', 'red'
+			);
+		});
+	},
 });
-
-
-//Detect marker found and lost
-AFRAME.registerComponent('registerevents', {
-		init: function () {
-			const marker = this.el;
-
-			marker.addEventListener("markerFound", ()=> {
-				var markerId = marker.id;
-				console.log('Marker Found: ', markerId);
-
-				document.getElementById('cursor').setAttribute(
-					'material', 'color', 'green'
-				);
-			});
-
-			marker.addEventListener("markerLost",() =>{
-				var markerId = marker.id;
-				console.log('Marker Lost: ', markerId);
-
-				document.getElementById('cursor').setAttribute(
-					'material', 'color', 'red'
-				);
-			});
-		},
-	});
